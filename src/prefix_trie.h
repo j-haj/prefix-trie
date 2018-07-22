738a015928eec8d78ed3b74612bc765b3300e5be
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-07-22 16:28:14 -0700

    Add signature for Suffix function

--- src/prefix_trie.h
+++ src/prefix_trie.h
@@ -1,28 +1,33 @@
 #ifndef PREFIX_TRIE_H__
 #define PREFIX_TRIE_H__
-#include <string>
 #include <memory>
+#include <set>
+#include <string>
 
 #include "trie_node.h"
 
 class PrefixTrie {
-  public:
+ public:
+  PrefixTrie() : root_(std::make_unique<TrieNode>()) {}
 
-    PrefixTrie() : root_(std::make_unique<TrieNode>()) {}
+  /**
+   * Inserts the string into the prefix trie. This method is idempotent.
+   */
+  void Insert(const std::string& s) noexcept;
 
-    /**
-     * Inserts the string into the prefix trie. This method is idempotent.
-     */
-    void Insert(const std::string& s) noexcept;
+  /**
+   * Check if prefix trie contains string.
+   */
+  bool Contains(const std::string& s) const noexcept;
 
-    /**
-     * Check if prefix trie contains string.
-     */
-    bool Contains(const std::string& s) const noexcept;
+  /**
+   * Returns a set of suffixes of strings whose prefix is `s`.
+   */
+  std::set<std::string> Suffixes(const std::string& s) const noexcept;
 
-  private:
-    std::unique_ptr<TrieNode> root_;
+ private:
+  std::unique_ptr<TrieNode> root_;
 
-}; // class PrefixTrie
+};  // class PrefixTrie
 
-#endif // PREFIX_TRIE_H__
+#endif  // PREFIX_TRIE_H__
